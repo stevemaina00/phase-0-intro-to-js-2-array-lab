a09a0f5a2e6515ab220bee718fe5847e2707ad47
Aastha <saxena.aastha91@gmail.com>
2022-08-03 15:04:29 -0400

    commit tree 9a88b0c74bc5edc3069ed737c811fae671bca389

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return cats
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name){
+    const newCats = [ ...cats,name]
+    return newCats
+}
+function prependCat(name){
+    const newCats = [ name,...cats]
+    return newCats
+}
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
